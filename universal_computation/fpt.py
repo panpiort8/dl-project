71f057953ad1ac1180af17bf872893376433d80e
Pan Piotr <rumcajsgajos@gmail.com>
2021-05-31 13:46:37 +0200

    Add ViT implementation

--- universal_computation/fpt.py
+++ universal_computation/fpt.py
@@ -1,5 +1,75 @@
-import torch
-import torch.nn as nn
+from transformers import GPT2Model
+
+from .ViT import *
+
+
+def get_nlp_transformer(model_name: str, pretrained: bool):
+    pretrained_transformer = GPT2Model.from_pretrained(model_name)
+    if pretrained:
+        transformer = pretrained_transformer
+    else:
+        transformer = GPT2Model(pretrained_transformer.config)
+
+    return transformer, transformer.config.n_embd
+
+
+class ViTEmbeddings(nn.Module):
+    def __init__(self, config, max_sequence_len: int):
+        super().__init__()
+        self.position_embeddings = nn.Parameter(torch.zeros(1, max_sequence_len + 1, config.hidden_size))
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+
+    def forward(self, embeddings):
+        bs = embeddings.shape[0]
+        seq_len = embeddings.shape[1]
+        cls_token = self.cls_token.expand(bs, -1, -1)
+        embeddings = torch.cat((cls_token, embeddings), dim=1)
+        embeddings = embeddings + self.position_embeddings[:, :seq_len + 1, :]
+        return embeddings
+
+
+class Lambda(nn.Module):
+    def forward(self, output):
+        x = output[0]
+        return x[:, 0]
+
+
+def get_vit_transformer(model_name: str, pretrained: bool, max_sequence_len: int = 1024):
+    config = CONFIGS[model_name]
+    model = VisionTransformer(config, num_classes=1000, img_size=384)
+    if pretrained:
+        model.load_from(np.load(f'{model_name}.npz'))
+    transformer = nn.Sequential(ViTEmbeddings(config, max_sequence_len), model.transformer.encoder, Lambda())
+    print(transformer)
+    return transformer, config.hidden_size
+
+
+def freeze_nlp_trans(transformer, *, freeze_ln, freeze_pos, freeze_ff, freeze_attn):
+    for name, p in transformer.named_parameters():
+        name = name.lower()
+        if 'ln' in name:
+            p.requires_grad = not freeze_ln
+        elif 'wpe' in name:
+            p.requires_grad = not freeze_pos
+        elif 'mlp' in name:
+            p.requires_grad = not freeze_ff
+        elif 'attn' in name:
+            p.requires_grad = not freeze_attn
+        else:
+            p.requires_grad = False
+
+
+def freeze_vit_trans(transformer, *, freeze_ln, freeze_ff, freeze_attn):
+    for name, p in transformer.named_parameters():
+        name = name.lower()
+        if '_norm' in name:
+            p.requires_grad = not freeze_ln
+        elif 'ffn.' in name:
+            p.requires_grad = not freeze_ff
+        elif 'attn.' in name:
+            p.requires_grad = not freeze_attn
+        else:
+            p.requires_grad = False
 
 
 class FPT(nn.Module):
@@ -23,6 +93,7 @@ class FPT(nn.Module):
             freeze_out=False,
             dropout=0.1,
             orth_gain=1.41,
+            max_sequence_len=1024,
     ):
         super().__init__()
 
@@ -36,28 +107,12 @@ class FPT(nn.Module):
         self.out_layer_sizes = [] if out_layer_sizes is None else out_layer_sizes
         self.dropout = dropout
 
-        if 'gpt' in model_name:
-            assert model_name in ['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']
-
-            from transformers import GPT2Model
-
-            pretrained_transformer = GPT2Model.from_pretrained(model_name)
-            if pretrained:
-                self.transformer = pretrained_transformer
-            else:
-                self.transformer = GPT2Model(pretrained_transformer.config)
-
-            if model_name == 'gpt2':
-                embedding_size = 768
-            elif model_name == 'gpt2-medium':
-                embedding_size = 1024
-            elif model_name == 'gpt2-large':
-                embedding_size = 1280
-            elif model_name == 'gpt2-xl':
-                embedding_size = 1600
-
+        if model_name.lower().startswith('gpt2'):
+            self.transformer, embedding_size = get_nlp_transformer(model_name, pretrained)
+        elif model_name.lower().startswith('vit'):
+            self.transformer, embedding_size = get_vit_transformer(model_name, pretrained, max_sequence_len)
         else:
-            raise NotImplementedError('model_name not implemented')
+            raise NotImplementedError()
 
         if use_embeddings_for_in:
             self.in_net = nn.Embedding(input_dim, embedding_size)
@@ -96,18 +151,17 @@ class FPT(nn.Module):
         self.out_net = nn.Sequential(*out_layers)
 
         if freeze_trans:
-            for name, p in self.transformer.named_parameters():
-                name = name.lower()
-                if 'ln' in name:
-                    p.requires_grad = not freeze_ln
-                elif 'wpe' in name:
-                    p.requires_grad = not freeze_pos
-                elif 'mlp' in name:
-                    p.requires_grad = not freeze_ff
-                elif 'attn' in name:
-                    p.requires_grad = not freeze_attn
-                else:
-                    p.requires_grad = False
+            if model_name.lower().startswith('gpt2'):
+                freeze_nlp_trans(self.transformer,
+                                 freeze_ln=freeze_ln,
+                                 freeze_pos=freeze_pos,
+                                 freeze_ff=freeze_ff,
+                                 freeze_attn=freeze_attn)
+            else:  # ViT
+                freeze_vit_trans(self.transformer,
+                                 freeze_ln=freeze_ln,
+                                 freeze_ff=freeze_ff,
+                                 freeze_attn=freeze_attn)
         if freeze_in:
             for p in self.in_net.parameters():
                 p.requires_grad = False
@@ -127,22 +181,26 @@ class FPT(nn.Module):
             ratio = 1
 
         x = self.in_net(x)
+        if self.model_name.lower().startswith('gpt2'):
+            transformer_outputs = self.transformer(
+                inputs_embeds=x,
+                return_dict=True,
+                output_attentions=output_attentions,
+            )
+            x = transformer_outputs.last_hidden_state
 
-        transformer_outputs = self.transformer(
-            inputs_embeds=x,
-            return_dict=True,
-            output_attentions=output_attentions,
-        )
-        x = transformer_outputs.last_hidden_state
+            if self.return_last_only:
+                x = x[:, -ratio:]
 
-        if self.return_last_only:
-            x = x[:,-ratio:]
-
-        x = self.out_net(x)
-        if self.return_last_only and ratio > 1:
-            x = x.reshape(x.shape[0], x.shape[1] // ratio, ratio * self.output_dim)
-
-        if output_attentions:
-            return x, transformer_outputs.attentions
-        else:
+            x = self.out_net(x)
+            if self.return_last_only and ratio > 1:
+                x = x.reshape(x.shape[0], x.shape[1] // ratio, ratio * self.output_dim)
+            if output_attentions:
+                return x, transformer_outputs.attentions
+            else:
+                return x
+        else:  # ViT
+            x = self.transformer(x)
+            x = self.out_net(x)
+            x = x.unsqueeze(1)
             return x
