7b353579be457b4f7013ed5dd84b79ec1dcf9229
piotrhm <helm.piotr@gmail.com>
2021-06-11 13:09:57 +0200

    fix

--- universal_computation/datasets/speech_commands.py
+++ universal_computation/datasets/speech_commands.py
@@ -13,6 +13,7 @@ class SpeechCommandsDataset(Dataset):
         super().__init__(*args, **kwargs)
 
         self.batch_size = batch_size  # we fix it so we can use dataloader
+        self.patch_size = patch_size
 
         self.transform = torchaudio.transforms.Resample(orig_freq=16000, new_freq=sample_rate)
 
@@ -41,10 +42,12 @@ class SpeechCommandsDataset(Dataset):
                 self.test_iter = iter(self.d_test)
                 x, y = next(self.test_iter)
 
+        x = self.transform(x)
+
         if self.patch_size is not None:
             x = rearrange(x, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=self.patch_size, p2=1)
 
-        x = self.transform(x).to(device=self.device)
+        x = x.to(device=self.device)
         y = y.to(device=self.device)
 
         self._ind += 1
